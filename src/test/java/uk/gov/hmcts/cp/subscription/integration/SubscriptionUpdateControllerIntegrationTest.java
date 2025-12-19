3a8202f43ed75e07665e9d93cf4d707a11773466
coling01 <colin.greenwood@hmcts.net>
2025-12-19 17:26:44 +0000

    fix: integration test expecting createdAt resolution to match up with expected

--- src/test/java/uk/gov/hmcts/cp/subscription/integration/SubscriptionUpdateControllerIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/cp/subscription/integration/SubscriptionUpdateControllerIntegrationTest.java
@@ -7,9 +7,12 @@ import tools.jackson.databind.ObjectMapper;
 import uk.gov.hmcts.cp.subscription.entities.ClientSubscriptionEntity;
 import uk.gov.hmcts.cp.subscription.model.EntityEventType;
 
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -25,7 +28,6 @@ class SubscriptionUpdateControllerIntegrationTest extends IntegrationTestBase {
     @Test
     void update_client_subscription_should_update_subscription() throws Exception {
         ClientSubscriptionEntity existing = insertSubscription("https://oldendpoint", List.of(EntityEventType.PCR));
-        String existingCreatedAt = existing.getCreatedAt().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
         String body = new ObjectMapper().writeValueAsString(request);
         mockMvc.perform(put("/client-subscriptions/{id}", existing.getId())
                         .contentType(MediaType.APPLICATION_JSON)
@@ -36,7 +38,11 @@ class SubscriptionUpdateControllerIntegrationTest extends IntegrationTestBase {
                 .andExpect(jsonPath("$.clientSubscriptionId").value(existing.getId().toString()))
                 .andExpect(jsonPath("$.eventTypes.[0]").value("CUSTODIAL_RESULT"))
                 .andExpect(jsonPath("$.eventTypes.[1]").value("PCR"))
-                .andExpect(jsonPath("$.notificationEndpoint.webhookUrl").value("https://my-callback-url"))
-                .andExpect(jsonPath("$.createdAt").value(existingCreatedAt + "Z"));
+                .andExpect(jsonPath("$.notificationEndpoint.webhookUrl").value("https://my-callback-url"));
+        verifyCreatedAtIsUnchanged(existing.getId(), existing.getCreatedAt());
+    }
+
+    void verifyCreatedAtIsUnchanged(UUID subscriptionId, OffsetDateTime expectedCreatedAt) {
+        assertThat(subscriptionRepository.findById(subscriptionId).get().getCreatedAt()).isEqualTo(expectedCreatedAt);
     }
 }
