eebc4d43535edbc4009d350c8633b09bb8f4b3d9
baran ipek <baran.ipek@hmcts.net>
2025-12-29 14:51:19 +0000

    amp-170: add api spec

---
+++ src/test/java/uk/gov/hmcts/cp/subscription/BaseIntegrationTest.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.cp.subscription;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public abstract class BaseIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:15-alpine")
+                    .withDatabaseName("appdb")
+                    .withUsername("postgres")
+                    .withPassword("postgres");
+
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "validate");
+        registry.add("spring.flyway.enabled", () -> "true");
+    }
+}
--- src/test/java/uk/gov/hmcts/cp/subscription/services/WebhookServiceTest.java
+++ src/test/java/uk/gov/hmcts/cp/subscription/services/WebhookServiceTest.java
@@ -4,42 +4,23 @@ import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.http.converter.autoconfigure.HttpMessageConvertersAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import uk.gov.hmcts.cp.openapi.model.PcrEventPayload;
-import uk.gov.hmcts.cp.subscription.client.DocumentServiceClient;
+import uk.gov.hmcts.cp.subscription.BaseIntegrationTest;
 
 import java.util.UUID;
 
-
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.containing;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
-@SpringBootTest(
-        classes = WebhookServiceTest.TestConfig.class,
-        webEnvironment = SpringBootTest.WebEnvironment.NONE
-)
-class WebhookServiceTest {
-
-    @Configuration
-    @EnableFeignClients(clients = DocumentServiceClient.class)
-    @Import({
-            FeignAutoConfiguration.class,
-            HttpMessageConvertersAutoConfiguration.class,
-            WebhookService.class
-    })
-    static class TestConfig {
-    }
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class WebhookServiceTest extends BaseIntegrationTest {
 
     @RegisterExtension
     static WireMockExtension wireMockServer = WireMockExtension.newInstance()
@@ -49,6 +30,7 @@ class WebhookServiceTest {
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
         registry.add("document-service.url", wireMockServer::baseUrl);
+
         registry.add("spring.cloud.discovery.enabled", () -> "false");
         registry.add("spring.cloud.service-registry.auto-registration.enabled", () -> "false");
     }
@@ -58,17 +40,17 @@ class WebhookServiceTest {
 
     @Test
     void should_call_document_service_to_update_metadata() {
-        wireMockServer.stubFor(post(urlEqualTo("/document-service/api/rest/document/metadata"))
-                .willReturn(aResponse()
-                        .withStatus(200)));
+        wireMockServer.stubFor(post(urlPathMatching("/document-service/api/rest/document/.*"))
+                .willReturn(aResponse().withStatus(200)));
 
-        PcrEventPayload payload = new PcrEventPayload();
-        payload.setEventId(UUID.randomUUID());
+        PcrEventPayload payload = PcrEventPayload.builder()
+                .eventId(UUID.randomUUID())
+                .build();
 
         webhookService.processPcrEvent(payload);
 
         wireMockServer.verify(postRequestedFor(
-                urlEqualTo("/document-service/api/rest/document/metadata"))
+                urlPathMatching("/document-service/api/rest/document/.*"))
                 .withHeader("Content-Type", containing("application/json")));
     }
 }
