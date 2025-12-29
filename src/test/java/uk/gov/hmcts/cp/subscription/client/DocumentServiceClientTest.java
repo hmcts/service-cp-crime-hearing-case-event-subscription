6f7f2df8da0970cbd2b6a19a996075ae2354119c
baran ipek <baran.ipek@hmcts.net>
2025-12-29 11:59:16 +0000

    feat:amp-170 add client to call webhook

--- src/test/java/uk/gov/hmcts/cp/subscription/client/DocumentServiceClientTest.java
+++ src/test/java/uk/gov/hmcts/cp/subscription/client/DocumentServiceClientTest.java
@@ -1,105 +1,45 @@
 package uk.gov.hmcts.cp.subscription.client;
 
-import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.http.converter.autoconfigure.HttpMessageConvertersAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import uk.gov.hmcts.cp.openapi.model.PcrEventPayload;
-
 import java.util.UUID;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.containing;
-import static com.github.tomakehurst.wiremock.client.WireMock.matchingJsonPath;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-@SpringBootTest(
-        classes = DocumentServiceClientTest.TestConfig.class,
-        webEnvironment = SpringBootTest.WebEnvironment.NONE
-)
 class DocumentServiceClientTest {
 
-    @Configuration
-    @EnableFeignClients(clients = DocumentServiceClient.class)
-    @Import({
-            FeignAutoConfiguration.class,
-            HttpMessageConvertersAutoConfiguration.class
-    })
-    static class TestConfig {
-    }
-
-    @RegisterExtension
-    static WireMockExtension wireMockServer = WireMockExtension.newInstance()
-            .options(wireMockConfig().dynamicPort())
-            .build();
-
-    @Autowired
     private DocumentServiceClient documentServiceClient;
 
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("document-service.url", wireMockServer::baseUrl);
-        registry.add("spring.cloud.discovery.enabled", () -> "false");
-        registry.add("spring.cloud.service-registry.auto-registration.enabled", () -> "false");
+    @BeforeEach
+    void setUp() {
+        documentServiceClient = mock(DocumentServiceClient.class);
     }
 
     @Test
-    void generateOpenApiModels_should_create_ErrorResponse() {
-        wireMockServer.stubFor(post(urlEqualTo("/document-service/api/rest/document/metadata"))
-                .willReturn(aResponse()
-                        .withStatus(204)));
+        void updateDocumentMetadata_should_callFeignClient() {
+            PcrEventPayload payload = new PcrEventPayload();
+            UUID eventId = UUID.randomUUID();
+            payload.setEventId(eventId);
 
-        PcrEventPayload payload = new PcrEventPayload();
-        payload.setEventId(UUID.randomUUID());
+            documentServiceClient.updateDocumentMetadata(payload);
 
-        documentServiceClient.updateDocumentMetadata(payload);
-
-        wireMockServer.verify(postRequestedFor(urlEqualTo("/document-service/api/rest/document/metadata"))
-                .withHeader("Content-Type", containing("application/json")));
+            verify(documentServiceClient, times(1)).updateDocumentMetadata(payload);
     }
 
     @Test
-    void should_handle_successful_response() {
-        wireMockServer.stubFor(post(urlEqualTo("/document-service/api/rest/document/metadata"))
-                .willReturn(aResponse()
-                        .withStatus(200)));
+    void updateDocumentMetadata_should_handle_multiple_calls() {
+        PcrEventPayload payload1 = new PcrEventPayload();
+        payload1.setEventId(UUID.randomUUID());
+        PcrEventPayload payload2 = new PcrEventPayload();
+        payload2.setEventId(UUID.randomUUID());
 
-        PcrEventPayload payload = new PcrEventPayload();
-        payload.setEventId(UUID.randomUUID());
+        documentServiceClient.updateDocumentMetadata(payload1);
+        documentServiceClient.updateDocumentMetadata(payload2);
 
-        documentServiceClient.updateDocumentMetadata(payload);
-
-        wireMockServer.verify(1, postRequestedFor(
-                urlEqualTo("/document-service/api/rest/document/metadata")));
-    }
-
-    @Test
-    void should_send_correct_payload_to_document_service() {
-        UUID eventId = UUID.randomUUID();
-
-        wireMockServer.stubFor(post(urlEqualTo("/document-service/api/rest/document/metadata"))
-                .willReturn(aResponse()
-                        .withStatus(204)));
-
-        PcrEventPayload payload = new PcrEventPayload();
-        payload.setEventId(eventId);
-
-        documentServiceClient.updateDocumentMetadata(payload);
-
-        wireMockServer.verify(postRequestedFor(urlEqualTo("/document-service/api/rest/document/metadata"))
-                .withHeader("Content-Type", containing("application/json"))
-                .withRequestBody(matchingJsonPath("$.eventId", equalTo(eventId.toString()))));
+        verify(documentServiceClient, times(1)).updateDocumentMetadata(payload1);
+        verify(documentServiceClient, times(1)).updateDocumentMetadata(payload2);
     }
 }
---
+++ src/test/java/uk/gov/hmcts/cp/subscription/client/DocumentServiceIntegrationTest.java
@@ -0,0 +1,105 @@
+package uk.gov.hmcts.cp.subscription.client;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.http.converter.autoconfigure.HttpMessageConvertersAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import uk.gov.hmcts.cp.openapi.model.PcrEventPayload;
+
+import java.util.UUID;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
+import static com.github.tomakehurst.wiremock.client.WireMock.matchingJsonPath;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@SpringBootTest(
+        classes = DocumentServiceIntegrationTest.TestConfig.class,
+        webEnvironment = SpringBootTest.WebEnvironment.NONE
+)
+class DocumentServiceIntegrationTest {
+
+    @Configuration
+    @EnableFeignClients(clients = DocumentServiceClient.class)
+    @Import({
+            FeignAutoConfiguration.class,
+            HttpMessageConvertersAutoConfiguration.class
+    })
+    static class TestConfig {
+    }
+
+    @RegisterExtension
+    static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .build();
+
+    @Autowired
+    private DocumentServiceClient documentServiceClient;
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("document-service.url", wireMockServer::baseUrl);
+        registry.add("spring.cloud.discovery.enabled", () -> "false");
+        registry.add("spring.cloud.service-registry.auto-registration.enabled", () -> "false");
+    }
+
+    @Test
+    void generateOpenApiModels_should_create_ErrorResponse() {
+        wireMockServer.stubFor(post(urlEqualTo("/document-service/api/rest/document/metadata"))
+                .willReturn(aResponse()
+                        .withStatus(204)));
+
+        PcrEventPayload payload = new PcrEventPayload();
+        payload.setEventId(UUID.randomUUID());
+
+        documentServiceClient.updateDocumentMetadata(payload);
+
+        wireMockServer.verify(postRequestedFor(urlEqualTo("/document-service/api/rest/document/metadata"))
+                .withHeader("Content-Type", containing("application/json")));
+    }
+
+    @Test
+    void should_handle_successful_response() {
+        wireMockServer.stubFor(post(urlEqualTo("/document-service/api/rest/document/metadata"))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+
+        PcrEventPayload payload = new PcrEventPayload();
+        payload.setEventId(UUID.randomUUID());
+
+        documentServiceClient.updateDocumentMetadata(payload);
+
+        wireMockServer.verify(1, postRequestedFor(
+                urlEqualTo("/document-service/api/rest/document/metadata")));
+    }
+
+    @Test
+    void should_send_correct_payload_to_document_service() {
+        UUID eventId = UUID.randomUUID();
+
+        wireMockServer.stubFor(post(urlEqualTo("/document-service/api/rest/document/metadata"))
+                .willReturn(aResponse()
+                        .withStatus(204)));
+
+        PcrEventPayload payload = new PcrEventPayload();
+        payload.setEventId(eventId);
+
+        documentServiceClient.updateDocumentMetadata(payload);
+
+        wireMockServer.verify(postRequestedFor(urlEqualTo("/document-service/api/rest/document/metadata"))
+                .withHeader("Content-Type", containing("application/json"))
+                .withRequestBody(matchingJsonPath("$.eventId", equalTo(eventId.toString()))));
+    }
+}
