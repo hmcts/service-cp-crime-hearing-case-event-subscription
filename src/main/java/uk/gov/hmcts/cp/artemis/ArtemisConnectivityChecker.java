c4780c01b9eb756b3289262d632e3f5fb2a8e4f1
Colin Greenwood <colingreenwood@scrumconnect.com>
2026-07-17 06:57:15 +0100

    AMP-868: add sslEnabled=true&verifyHost=false to match broker TLS config (no client auth required)

--- src/main/java/uk/gov/hmcts/cp/artemis/ArtemisConnectivityChecker.java
+++ src/main/java/uk/gov/hmcts/cp/artemis/ArtemisConnectivityChecker.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class ArtemisConnectivityChecker {
 
     private static final int PORT = 61_616;
-    private static final String CONNECTION_TIMEOUT_PARAMS = "call_timeout=5000&connection_ttl=5000&initial_connect_attempts=1&reconnect_attempts=0";
+    private static final String CONNECTION_TIMEOUT_PARAMS = "sslEnabled=true&verifyHost=false&call_timeout=5000&connection_ttl=5000&initial_connect_attempts=1&reconnect_attempts=0";
 
     @Value("${artemis.host.primary}")
     private String primaryHost;
