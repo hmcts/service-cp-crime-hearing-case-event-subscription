874856557258f7e6379f977dbbfb0336f25905e9
Colin Greenwood <colingreenwood@scrumconnect.com>
2026-07-17 13:41:04 +0100

    AMP-865: exclude high-volume getHearingEvent and getDocument from audit

--- src/main/java/uk/gov/hmcts/cp/subscription/controllers/NotificationController.java
+++ src/main/java/uk/gov/hmcts/cp/subscription/controllers/NotificationController.java
@@ -23,7 +23,6 @@ import uk.gov.hmcts.cp.openapi.api.NotificationApi;
 import uk.gov.hmcts.cp.openapi.model.EventPayload;
 import uk.gov.hmcts.cp.openapi.model.HearingEventResponse;
 import uk.gov.hmcts.cp.servicebus.services.ServiceBusClientService;
-import uk.gov.hmcts.cp.audit.annotation.AuditDetail;
 import uk.gov.hmcts.cp.audit.annotation.AuditExclude;
 import uk.gov.hmcts.cp.subscription.config.AppProperties;
 import uk.gov.hmcts.cp.subscription.config.EnvironmentName;
@@ -48,7 +47,6 @@ import static uk.gov.hmcts.cp.servicebus.config.ServiceBusProperties.NOTIFICATIO
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@SuppressWarnings("PMD.AvoidDuplicateLiterals")
 public class NotificationController implements InternalApi, NotificationApi {
 
     private final ServiceBusClientService clientService;
@@ -100,7 +98,7 @@ public class NotificationController implements InternalApi, NotificationApi {
     }
 
     @Override
-    @AuditDetail(eventName = "hrds.get-hearing-event", pathParams = {"clientSubscriptionId", "hearingEventId"})
+    @AuditExclude
     public ResponseEntity<HearingEventResponse> getHearingEvent(
             @NotNull @PathVariable("clientSubscriptionId") final UUID clientSubscriptionId,
             @NotNull @PathVariable("hearingEventId") final UUID hearingEventId,
@@ -117,7 +115,7 @@ public class NotificationController implements InternalApi, NotificationApi {
     }
 
     @Override
-    @AuditDetail(eventName = "hrds.get-document", action = "Download", pathParams = {"clientSubscriptionId", "documentId"})
+    @AuditExclude
     public ResponseEntity<Resource> getDocument(
             @NotNull @PathVariable("clientSubscriptionId") final UUID clientSubscriptionId,
             @NotNull @PathVariable("documentId") final UUID documentId,
